29da2bff5ae7f4d7ee6015152e876733f4090920
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2023-07-24 06:27:28 +0000

    mmm

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -125,7 +125,7 @@ async def gen_thumb(videoid, user_id):
         try:
             draw.text(
                 (450, 25),
-                f"LH KING",
+                f"LH",
                 fill="white",
                 stroke_width=3,
                 stroke_fill="grey",
